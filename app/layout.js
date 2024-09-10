475719f72f01faaf06aa9928420120526ee43087
Muhammad Usman <151831823+Muhammad-Usman-07@users.noreply.github.com>
2024-09-10 14:50:38 +0500

    six commit

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,13 +16,14 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
 
-      <Navbar />
-      <div className="bg-gray-800 min-h-screen">
-        {children}
-      </div>
-      <Footer />
-        
-        </body>
+        <Navbar />
+        <div className="bg-gray-800 min-h-screen">
+          {children}
+        </div>
+        <Footer />
+        <Analytics />
+
+      </body>
     </html>
   );
 }
